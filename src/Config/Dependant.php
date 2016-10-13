33d493f9fd5a04ec74270aef3d4b67c7b4939560
Adam Kowalski <adam.kowalski@miinto.com>
2016-10-13 15:43:19 +0200

    Initial commit

---
+++ src/Config/Dependant.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * This trait provides functionality common to all classes dependant on the \Maleficarum\Config namespace
+ */
+
+namespace Maleficarum\Config;
+
+trait Dependant
+{
+    /**
+     * Internal storage for the config object.
+     *
+     * @var \Maleficarum\Config\AbstractConfig
+     */
+    protected $configStorage = null;
+
+    /**
+     * Inject a new config object.
+     *
+     * @param \Maleficarum\Config\AbstractConfig $config
+     *
+     * @return $this
+     */
+    public function setConfig(\Maleficarum\Config\AbstractConfig $config)
+    {
+        $this->configStorage = $config;
+
+        return $this;
+    }
+
+    /**
+     * Fetch the currently assigned config object.
+     *
+     * @return \Maleficarum\Config\AbstractConfig|null
+     */
+    public function getConfig()
+    {
+        return $this->configStorage;
+    }
+
+    /**
+     * Detach the currently assigned config object.
+     *
+     * @return $this
+     */
+    public function detachConfig()
+    {
+        $this->configStorage = null;
+
+        return $this;
+    }
+}
